7b58c67909844d531e2c32f6436b4b35a1182aab
cesartellezornelas <cesar3tellez@gmail.com>
2025-04-16 19:18:57 -0700

    Initial Commit

---
+++ AdditionCalculator/AdditionCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdditionCalculatorApp.swift
+//  AdditionCalculator
+//
+//  Created by Cesar Tellez Ornelas on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct AdditionCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdditionCalculatorTests/AdditionCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  AdditionCalculatorTests.swift
+//  AdditionCalculatorTests
+//
+//  Created by Cesar Tellez Ornelas on 4/16/25.
+//
+
+import Testing
+@testable import AdditionCalculator
+
+struct AdditionCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
